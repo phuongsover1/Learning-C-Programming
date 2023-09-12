ae8ec2e3697802db183ac23a09063a7efad45597
Phuong Nguyen <phuongsover2@gmail.com>
2023-09-12 20:20:56 +0700

    function prototype

---
+++ 15. Functions/function-prototype.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double wa(double x, double y);
+
+int main() {
+  double a,b,sum;
+  a = 11.11;
+  b = 22.22;
+  sum = wa(a,b);
+  return 0;
+}
+wa1(void) {
+
+}
+
+double wa(double x, double y) {
+  double z;
+  z = x + y;
+  return z;
+}
