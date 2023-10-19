9567df41bc57f74a5746112b23d92472caeadec3
Phuong Nguyen <phuongsover2@gmail.com>
2023-10-19 10:25:32 +0700

    complete challenge#1

---
+++ 28.Dynamic-memory-allocation/challenge-#1-creating-and-returning-a-dynamic-allocated-array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *createdArray(int size);
+void printArray(int *arr, int size);
+int main(void) {
+  int size;
+  printf("Enter size: ");
+  scanf("%d", &size);
+  int *arr = createdArray(size);
+  if (arr != NULL) {
+    printArray(arr, size);
+  } else {
+    puts("Failed to allocate array\n");
+  }
+  return 0;
+}
+
+int *createdArray(int size) {
+  int *returnedArray = (int *)malloc(sizeof(int) * size);
+  if (returnedArray != NULL) {
+    for (int i = 0; i < size; i++) {
+      printf("Enter arr[%d] = ", i);
+      scanf("%d", &returnedArray[i]);
+    }
+  }
+  return returnedArray;
+}
+
+void printArray(int *arr, int size) {
+  while (size > 0) {
+    printf("%d ", *arr);
+    arr++;
+    size--;
+  }
+}
