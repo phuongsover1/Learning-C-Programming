1cc5bd5dcfd0d9e5d69795e398efeeb8062f0ee3
Phuong Nguyen <phuongsover2@gmail.com>
2023-10-23 16:16:23 +0700

    check union values

--- 24.Unions/4.further-example.c
+++ 24.Unions/4.further-example.c
@@ -3,7 +3,6 @@
 union Student {
   int ID;
   double average;
-
 };
 int main(void) {
   union Student student1;
@@ -14,6 +13,8 @@ int main(void) {
   ptrStudent->ID = 3;
 
   printf("Student's ID: %d\n", student1.ID);
-
+  ptrStudent->average = 3.4;
+  printf("Student's ID: %d\n", student1.ID);
+  printf("Student's Average: %lf\n", student1.average);
   return 0;
 }
